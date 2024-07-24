094263e2ee674b1b9591eb14a9e90ad4a1d49440
Akintomiwa Peter Adedokun <143067882+Akintomiwa200@users.noreply.github.com>
2024-07-24 11:31:46 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 function toggleMenu() {
-    const menu = document.querySelector(".menu-links");
-    const icon = document.querySelector(".hamburger-icon");
+    const menu = document.querySelector("#hamburger-nav .menu-links");
+    const icon = document.querySelector("#hamburger-nav .hamburger-icon");
+
     menu.classList.toggle("open");
     icon.classList.toggle("open");
 }
